342a0728a24dfebf1d54965df63f76b92dae5e14
fathy mohamed <fathymohamedfathy299@gmail.com>
2026-05-24 19:01:53 +0300

    add user route

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const session = require("express-session");
 const moment = require("moment");
 const dotenv = require("dotenv");
-const compression = require("compression");
 
 const Customer = require("./modle/customer");
 const mongoose = require("./config/db");
@@ -12,13 +11,11 @@ const authRoutes = require("./routes/authRoutes");
 const logRoutes = require("./routes/logRoutes");
 const userRoutes = require("./routes/userRoutes");
 const signout = require("./routes/Signout");
-
 dotenv.config();
-
-const app = express(); 
-
+const compression = require("compression");
 
 app.use(compression());
+const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -32,26 +29,77 @@ app.use(session({
   }
 }));
 
-// view engine
+
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-// routes
+
 app.use("/", authRoutes);
 app.use("/", logRoutes);
 app.use("/", signout);
 app.use("/", userRoutes);
 
-// home route
-app.get("/", (req, res) => {
-  if (!req.session.user) {
-    return res.redirect("/login");
+
+app.post("/user/add", async (req, res) => {
+  try {
+    await Customer.create({
+      ...req.body,
+      userId: req.session.user?.id
+    });
+
+    res.redirect("/index");
+
+  } catch (err) {
+    console.log(err);
+    res.send("Error occurred");
   }
+});
+
+
+app.get("/user/:id", async (req, res) => {
+  const user = await Customer.findById(req.params.id);
+
+  res.render("user/view", {
+    user,
+    moment,
+    name: req.session.user?.name
+  });
+});
+
+
+
+app.post("/user/delete/:id", async (req, res) => {
+  await Customer.findByIdAndDelete(req.params.id);
+  res.redirect("/index");
+});
+
+
+
+app.post("/user/update/:id", async (req, res) => {
+  await Customer.findByIdAndUpdate(req.params.id, req.body);
+  res.redirect("/index");
+});
+
+
+
+app.get("/search", async (req, res) => {
+  const users = await Customer.find({
+    FirstName: {
+      $regex: req.query.search,
+      $options: "i"
+    }
+  });
 
   res.render("index", {
-    name: req.session.user.name,
-    moment
+    arr: users,
+    moment,
+    name: req.session.user?.name
   });
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
 });
 
 
@@ -59,5 +107,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on ${PORT}`);
-});
-mongoose()
+}); 
+mongoose();
